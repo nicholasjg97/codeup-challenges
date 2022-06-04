b5278b978286f33ac4e56e1802c6b199c9f6cbb5
Nico Gaytan <nicholas.j.gaytan97@gmail.com>
2022-06-03 23:38:16 -0500

    fixed previous and added problem

--- codeup-intermediate-challenges.js
+++ codeup-intermediate-challenges.js
@@ -13,14 +13,44 @@ function decFreq(string = '') {
        charFreqMap[char] = isCharInCharMap ? charFreqMap[char] + 1 : 1;
     }
 
-    // Create frequency array freq[]
-    var freq = new Array(maxE + 1).fill(0);
-
-    // Update the frequency array as per the occurrence of element in array[]
-    for (var i = 0; i < n; i++) {
-        freq[arr[i]]++;
+    const charArray = [];
+    for (let char in charFreqMap) {
+        const frequency = charFreqMap[char];
+        const extrapolatedString = char.repeat(frequency);
     }
+    charArray.sort((a, b) => b.length - a.length);
 
-    // Initialize cnt to 0;
-
+    return charArray.join('');
 }
+
+// Return the length of the longest possible palindrome within a given string.
+
+// Examples:
+// Input: "bbadbb" | Output:5 (bbabb or bbdbb)
+// Input: "bcd" | Output: 1 (b, c, or d)
+
+function longestPalindrome(s) {
+    if (s.length < 1) return "";
+    let maxSubStart = 0;
+    let maxSubLength = 0;
+    for (let i = 0; i < s.length; i++) {
+      const lengthCenteredAtChar = expandAroundCenter(s, i, i);
+      const lengthCenteredAtSpace = expandAroundCenter(s, i, i + 1);
+      const longestSubAtChar = Math.max(lengthCenteredAtChar, lengthCenteredAtSpace)
+      if (longestSubAtChar > maxSubLength) {
+        maxSubLength = longestSubAtChar;
+        maxSubStart = i - Math.floor((maxSubLength - 1) / 2);
+      }
+    }
+    return s.substr(maxSubStart, maxSubLength);
+  }
+  
+//   https://dev.to/alisabaj/the-longest-palindromic-substring-solving-the-problem-using-constant-space-1bch
+
+  function expandAroundCenter(s, left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
+    }
+    return right - left - 1;
+  }
